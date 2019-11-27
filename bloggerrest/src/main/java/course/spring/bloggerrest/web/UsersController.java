c03a678d3adaf636831cee23b325ab930b7391b8
Trayan D Troev <trayan97@gmail.com>
2019-11-27 13:31:39 +0200

    Implemented Validation handling inside Users` controller.

--- bloggerrest/src/main/java/course/spring/bloggerrest/web/UsersController.java
+++ bloggerrest/src/main/java/course/spring/bloggerrest/web/UsersController.java
@@ -5,8 +5,9 @@ import course.spring.bloggerrest.exception.InvalidEntityException;
 import course.spring.bloggerrest.model.User;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
-import org.springframework.web.servlet.mvc.method.annotation.MvcUriComponentsBuilder;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import javax.validation.Valid;
 import java.util.List;
@@ -17,30 +18,42 @@ public class UsersController {
     @Autowired
     UsersService service;
 
-
     @GetMapping
     List<User> getUsers(){
         return service.findAll();
     }
 
     @GetMapping("{userId}")
-    User getPostById(@PathVariable("userId") String postId) {
-        return service.findById(postId);
+    User getUserById(@PathVariable("userId") String userId) {
+        return service.findById(userId);
     }
 
     @PostMapping
-    ResponseEntity<User> addPost(@Valid @RequestBody User user) {
+    ResponseEntity<User> addUser(@Valid @RequestBody User user, BindingResult bindingResult) {
+        if (bindingResult.hasFieldErrors()) {
+            String message = bindingResult.getFieldErrors().stream()
+                    .map(err -> String.format("Invalid %s -> %s\n",
+                            err.getField(), err.getDefaultMessage()))
+                    .reduce("", (acc, err) -> acc + err);
+
+            throw new InvalidEntityException(message);
+        }
         User created = service.add(user);
-        return ResponseEntity.created(MvcUriComponentsBuilder
-                .fromMethodName(PostsController.class, "addUser", User.class)
-                .pathSegment("{id}").build(created.getId())).body(created);
+        return ResponseEntity.created(
+                ServletUriComponentsBuilder.fromCurrentRequest()
+                        .pathSegment("{id}").build(created.getId())).body(created);
     }
 
     @PutMapping("{userId}")
-    public User update (@PathVariable String userId, @Valid @RequestBody User user) {
-//        if (errors.hasErrors()) {
-//            throw new InvalidEntityException(errors.toString());
-//        }
+    public User update (@PathVariable String userId, @Valid @RequestBody User user, BindingResult bindingResult) {
+        if (bindingResult.hasFieldErrors()) {
+            String message = bindingResult.getFieldErrors().stream()
+                    .map(err -> String.format("Invalid %s -> %s\n",
+                            err.getField(), err.getDefaultMessage()))
+                    .reduce("", (acc, err) -> acc + err);
+
+            throw new InvalidEntityException(message);
+        }
         if (!userId.equals(user.getId())) {
             throw new InvalidEntityException(
                     String.format("Entity ID=\"%s\" is different from URL resource ID\"%s\"",
